921473321e63d9bd4806a6b02f42ae9bba966b61
deepak <deepak@excellencetechnologies.in>
2020-10-21 12:07:12 +0530

    layout1

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Layout1</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="index.css">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="navigation"> 
+    <li><a class="navigationlink1" href="#">Layout1</a></li>
+    <li><a class="navigationlink2" href="#">About</a></li>
+    <li><a class="navigationlink3" href="#">Service</a></li>
+    <li><a class="navigationlink4" href="#">Contact</a></li>
+</div>
+<div class="middlediv">
+  <div class="innerdiv">
+  <div class="styleheadingpara">
+  <h2 class="hstyle"> Page Heading </h2>
+  <p class="pstyle"> Seconadry Text </p>
+</div>
+<div  class="line"></div> 
+<div class="firstrow">
+   <div class="frstdiv1">
+     <img src ="div.png" class="imgstyle">
+     <h4 class="headingstyle">Project Name <h4>
+     <p class="paragraphstyle"> Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+   </div>
+   <div class="frstdiv2">
+     <img src ="div.png" class="imgstyle">
+     <h4 class="headingstyle">Project Name <h4>
+     <p class="paragraphstyle"> Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+   </div>
+   <div class="frstdiv3">
+     <img src ="div.png" class="imgstyle">
+     <h4 class="headingstyle">Project Name <h4>
+     <p class="paragraphstyle"> Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+   </div>
+</div> 
+<div class="firstrow">
+   <div class="frstdiv1">
+     <img src ="div.png" class="imgstyle">
+     <h4 class="headingstyle">Project Name <h4>
+     <p class="paragraphstyle"> Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+   </div>
+   <div class="frstdiv2">
+     <img src ="div.png" class="imgstyle">
+     <h4 class="headingstyle">Project Name <h4>
+     <p class="paragraphstyle"> Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+   </div>
+   <div class="frstdiv3">
+     <img src ="div.png" class="imgstyle">
+     <h4 class="headingstyle">Project Name <h4>
+     <p class="paragraphstyle"> Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+   </div>
+</div> 
+
+<div class="firstrow">
+   <div class="frstdiv1">
+     <img src ="div.png" class="imgstyle">
+     <h4 class="headingstyle">Project Name <h4>
+     <p class="paragraphstyle"> Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+   </div>
+   <div class="frstdiv2">
+     <img src ="div.png" class="imgstyle">
+     <h4 class="headingstyle">Project Name <h4>
+     <p class="paragraphstyle"> Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+   </div>
+   <div class="frstdiv3">
+     <img src ="div.png" class="imgstyle">
+     <h4 class="headingstyle">Project Name <h4>
+     <p class="paragraphstyle"> Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+   </div>
+</div>
+<div class="footer">
+<div  class="line"></div> <br/>
+<div class="copyright"> 
+  <p class="copyrighttext">  Copyright © Your Website 2014   </p>
+</div>
+  </div>
+</div>
+</body>
+</html>
